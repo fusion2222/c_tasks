7b73cb3df5277141af150b33c41dce0494d8ebd1
Matej Šrubař <matosrubar@gmail.com>
2019-08-07 13:46:09 +0200

    finished parking field task

--- parking_field.c
+++ parking_field.c
@@ -1,6 +1,24 @@
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+const int MAX_COMMAND_BUFFER_LENGTH = 20;
+
+
+void show_total_free_parking_spaces(unsigned int * parking_field, int * total_parking_places){
+	// typeof in this case returns 4 on some OS, 8 on mac, which is damn wierd!!!
+	int total_parking_field_bits = sizeof(unsigned int) * 8;
+	unsigned int mask = 1;
+	int total_free_places = 0;
+
+	for (int i = 0; i < *total_parking_places; ++i){
+		total_free_places += (*parking_field) >> i & mask;
+	}
+
+	printf("There are %d free parking places available.\n\n", total_free_places);
+}
 
 
 void show_parking_field(unsigned int * parking_field, int * total_parking_places){
@@ -8,45 +26,107 @@ void show_parking_field(unsigned int * parking_field, int * total_parking_places
 	int total_parking_field_bits = sizeof(unsigned int) * 8;
 	unsigned int mask = 1;
 
-	printf("==============\n");
-	printf("Parking places\n");
+	int output_seg_size = 3;
+
+	char solid_output_line[*total_parking_places * output_seg_size + 1];
+	char places_output_line[*total_parking_places * output_seg_size + 1];
+	
 
 	for (int i = 0; i < *total_parking_places; ++i){
-		printf("==============\n");
-		printf("%d\n", (*parking_field) >> i & mask);
+		solid_output_line[i * output_seg_size + 2] = '=';
+		solid_output_line[i * output_seg_size + 1] = '=';
+		solid_output_line[i * output_seg_size] = '=';
+
+		places_output_line[i * output_seg_size + 2] = ' ';
+		places_output_line[i * output_seg_size + 1] = (char) ((*parking_field) >> i & mask) + 48;
+		places_output_line[i * output_seg_size] = ' ';
 	}
 
+	
+	printf("%s\n", solid_output_line);
+	printf("Parking field\n");
+	printf("%s\n", solid_output_line);
+	printf("%s\n", places_output_line);
+	printf("%s\n\n", solid_output_line);
+	
 }
 
 
-char * get_user_command(){
-	printf(
-		"Please enter the command. These are available:\n"
-		" - check {parking place number} - Checks if specified parking place is free or not.\n"
-		" - capture {parking place number} - Marks specified parking place as captured.\n"
-		" - free {parking place number} - Frees parking specified place.\n"
-		" - show - Shows graphical parking field representation.\n"
-		" - total_free - Shows count total free parking places.\n"
-	);
+void is_parking_place_free(unsigned int * parking_field, int parking_place){
+	int total_parking_field_bits = sizeof(unsigned int) * 8;
+	unsigned int mask = 1;
+	printf("Parking place number %d is %s\n\n", parking_place, (*parking_field) >> parking_place & mask ? "occupied" : "free");
+}
 
-	int char_command_length = 20;
-	char * command[char_command_length];
+void capture_parking_place(unsigned int * parking_field, int parking_place){
+	int total_parking_field_bits = sizeof(unsigned int) * 8;
+	unsigned int mask = 1;
+	*parking_field = (mask << parking_place) | *parking_field;
+	printf("Parking field number %d has been captured!\n\n", parking_place);
+}
 
-	while(i){
-		scanf(" %s", command);
+void free_parking_place(unsigned int * parking_field, int parking_place){
+	int total_parking_field_bits = sizeof(unsigned int) * 8;
+	unsigned int mask = 1;
+	*parking_field = (mask << parking_place) ^ *parking_field;
+	printf("Parking field number %d has been freed!\n\n", parking_place);
+}
 
-		if(strcmp(command, "total_free") || strcmp(command, "show")){
-			// No further param commands.
+
+char * ask_and_execute_user_command(unsigned int * parking_field, int * total_parking_places){
+	char command[MAX_COMMAND_BUFFER_LENGTH];
+	
+	while(1){
+		printf(
+			"Please enter the command. These are available:\n"
+			" - check {parking place number} - Checks if specified parking place is free or not.\n"
+			" - capture {parking place number} - Marks specified parking place as captured.\n"
+			" - free {parking place number} - Frees parking specified place.\n"
+			" - show - Shows graphical parking field representation.\n"
+			" - total_free - Shows count total free parking places.\n"
+			" - exit - Exits the program.\n"
+		);
+
+		memset(command, '\0', sizeof(command));
+		fgets(command, MAX_COMMAND_BUFFER_LENGTH, stdin); 
+
+		if(strcmp(command, "exit\n") == 0){
+			printf("Exiting program... Bye!\n");
+			exit(0);
+		}else if(strcmp(command, "show\n") == 0){
+			show_parking_field(parking_field, total_parking_places);
+			continue;
+		}else if(strcmp(command, "total_free\n") == 0){
+			show_total_free_parking_spaces(parking_field, total_parking_places);
+			continue;
+		}
+
+		for (int i = 0; i < MAX_COMMAND_BUFFER_LENGTH; i++){
+			if(command[i] != ' '){continue;}
+
+			char * number_param_ptr = command + i + 1;
+			int numeric_parameter = atoi(number_param_ptr);
+			command[i] = '\0';
+
+			if(numeric_parameter < 0 || numeric_parameter > *total_parking_places){
+				printf("Parking available places indexes can are only from 0 to is %d!!!\n\n", *total_parking_places);
+				break;
+			}
+
+			if(strcmp(command, "check") == 0){
+				is_parking_place_free(parking_field, numeric_parameter);
+				break;
+			}else if(strcmp(command, "capture") == 0){
+				capture_parking_place(parking_field, numeric_parameter);
+				break;
+			}else if(strcmp(command, "free") == 0){
+				free_parking_place(parking_field, numeric_parameter);
+				break;
+			}
+
 			break;
 		}
-
-		for (int i = 0; i < char_command_length; ++i){
-			if(command[i] == ' '){
-				// i
-			}
-		}
 	}
-
 }
 
 
@@ -64,9 +144,9 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	unsigned int parking_field = 0;
+	unsigned int parking_field = 1;
 
-	show_parking_field(&parking_field, &total_parking_places);
+	ask_and_execute_user_command(&parking_field, &total_parking_places);
 
 	return 0;
 }
