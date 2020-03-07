3e174f255654899fed2cd95161a0df73d3493553
Matej Šrubař <matosrubar@gmail.com>
2020-03-07 17:36:49 +0100

    separate count_sort function

--- count_sort.c
+++ count_sort.c
@@ -3,11 +3,7 @@
 #include "utils.h"
 
 
-int main(int argc, char const *argv[]){
-	int sortlist[] = {4, 1, 1, 3, 0, 5, 2, 6, 2, 8, 7, 9, 3, 1};
-	int sortlist_length = (int)sizeof(sortlist) / (int)sizeof(sortlist[0]);
-
-	display_results(sortlist, sortlist_length);
+void count_sort(int * sortlist, int * output_sortlist, int sortlist_length){
 
 	// Initialize counting array with zeroes
 	int count_array_length = sortlist_length + 1;
@@ -24,10 +20,6 @@ int main(int argc, char const *argv[]){
 		count_array[i] = count_array[i] + count_array[i - 1];
 	}
 
-	// Create output array
-	int output_sortlist[sortlist_length];
-	memset(output_sortlist, 0, sortlist_length * (int)sizeof(int));
-
 	for(int i = 0; i < sortlist_length; ++i){
 		count_array[sortlist[i]]--;
 		output_sortlist[
@@ -35,6 +27,22 @@ int main(int argc, char const *argv[]){
 		] = sortlist[i];
 	}
 
+}
+
+
+int main(int argc, char const *argv[]){
+	int sortlist[] = {4, 1, 1, 3, 0, 5, 2, 6, 2, 8, 7, 9, 3, 1};
+	int sortlist_length = (int)sizeof(sortlist) / (int)sizeof(sortlist[0]);
+
+	display_results(sortlist, sortlist_length);
+
+	// Create output array
+	int output_sortlist[sortlist_length];
+	memset(output_sortlist, 0, sortlist_length * (int)sizeof(sortlist[0]));
+
+	// Perform countsort
+	count_sort(sortlist, output_sortlist, sortlist_length);
 	display_results(output_sortlist, sortlist_length);
+
 	return 0;
 }
